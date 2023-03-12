9abb7874d2572760151d1433612a30ff729291fb
AliyevMura <7lvghl9@code.edu.az>
2023-03-12 12:00:01 +0400

    Initial commit

---
+++ c#/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Task 1
+
+//using System;
+//int num = 123;
+//int result = 0;
+//while (num > 0)
+//{
+//    result = result * 10 + num % 10;
+//    num /= 10;
+//}
+//return result;
+
+
+
+//Task 2
+
+
+//int n = int.Parse(Console.ReadLine());
+//int x = n;
+//int k = 0;
+//int i = 10;
+//while(x % 10 !=x)
+//{
+//    k++;
+//    x /= 10;
+//    i = i * 10;
+//}
+//Console.WriteLine(n+(3*i));
+//Console.ReadLine();
